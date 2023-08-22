a42dab7494d00356837ef24452e7c74e9881d294
Tran Trung Hau <trunghau2003kg@gmail.com>
2023-08-22 22:40:34 +0700

    reactcomponet

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Welcome from "./Welcome";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const elenment11 = <h1>Hello react</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+function tick() {
+  const element = <Welcome name="hau" />;
+  root.render(element);
+}
+
+setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
